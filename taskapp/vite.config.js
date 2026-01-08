f6105e0db4b542529cad3ceb9c9510f1761770ca
nrau22 <niko.rautio@centria.fi>
2026-01-08 16:27:30 +0200

    render

--- taskapp/vite.config.js
+++ taskapp/vite.config.js
@@ -4,10 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, // <-- allow network access (phone/devices)
-    port: 5173, // optional, Vite port
     proxy: {
-      '/api': 'http://localhost:3000', // <-- forward all /api calls to Express
-    },
-  },
+      '/api': 'http://localhost:3000' // forward API calls to Express
+    }
+  }
 });
